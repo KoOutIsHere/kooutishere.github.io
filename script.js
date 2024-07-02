d5a60b5ece8693e72ea4179e99fe28e3d8fff7b3
KoOutIsHere <fikopia@live.com>
2024-07-02 21:18:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById('addressinput').addEventListener('input', async function() {
     const query = this.value;
-    if (query.length < 10) {
+    if (query.length < 4) {
         document.getElementById('suggestions').innerHTML = '';
         return;
     }

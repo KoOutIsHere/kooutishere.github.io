27d112e549aed17bd5c94e2bb82d9872c081de26
KoOutIsHere <fikopia@live.com>
2024-07-02 21:01:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ document.getElementById('address').addEventListener('input', async function() {
     }
 
     try {
-        const response = await fetch(`https://nominatim.openstreetmap.org/search?addressdetails=1&countrycodes=HK&q=${query}&format=json`);
+        const response = await fetch(`https://nominatim.openstreetmap.org/search?addressdetails=1&q=bakery+in+berlin+wedding&format=jsonv2&limit=1`);
         const suggestions = await response.json();
         const suggestionsList = suggestions.map(item => 
             `<div class="suggestion-item" data-address="${item.display_name}">${item.display_name}</div>`
